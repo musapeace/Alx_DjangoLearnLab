031d6d5f12c459f2b485e05c1d4e4322691fb6a1
musapeace <mubarakinda@gmail.com>
2025-03-01 20:29:42 +0000

    Implementing a Custom User Model in Django

---
+++ advanced_features_and_security/LibraryProject/accounts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.contrib.auth import get_user_model
+# Create your views here.
+
+User = get_user_model()
+
+def some_view(request):
+    user = User.objects.get(id=1)
+    
+
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
 
 # Create your models here.
 class Book(models.Model):
@@ -22,3 +24,38 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
+    
+
+class CustomUserManager(BaseUserManager):
+    """Custom manager for the CustomUser model."""
+
+    def create_user(self, username, email, date_of_birth, password=None, **extra_fields):
+        """Creates and returns a regular user."""
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not date_of_birth:
+            raise ValueError("Users must provide their date of birth")
+
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, date_of_birth=date_of_birth, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, date_of_birth, password=None, **extra_fields):
+        """Creates and returns a superuser."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(username, email, date_of_birth, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    """Custom User model extending Django's AbstractUser."""
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
