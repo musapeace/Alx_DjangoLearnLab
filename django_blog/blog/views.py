47865bbcbf657a846283d86acd4b991b52210345
musapeace <mubarakinda@gmail.com>
2025-03-23 00:21:41 +0000

    Implementing the Blog's User Authentication System

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from .forms import UserRegisterForm
+from django.contrib.auth.forms import UserChangeForm
 
-# Create your views here.
+# User Registration View
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful!")
+            return redirect("blog-home")
+    else:
+        form = UserRegisterForm()
+    
+    return render(request, "blog/register.html", {"form": form})
+
+# User Login View
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, "Login successful!")
+            return redirect("blog-home")
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, "blog/login.html", {"form": form})
+
+# User Logout View
+def user_logout(request):
+    logout(request)
+    messages.info(request, "Logged out successfully.")
+    return redirect("login")
+
+# User Profile View
+def profile_view(request):
+    return render(request, "blog/profile.html", {"user": request.user})
+
+
+
+def edit_profile(request):
+    if request.method == "POST":
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
+    else:
+        form = UserChangeForm(instance=request.user)
+    return render(request, "blog/edit_profile.html", {"form": form})
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'blog/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,9 +127,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'blog/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'profile'
+LOGOUT_REDIRECT_URL = 'login'
+
